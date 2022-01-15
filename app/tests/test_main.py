353f58bd1ce8e72f367eca9809a09913675c6df2
hsnu130319@gmail.com <42515315+Joanne03@users.noreply.github.com>
2022-01-15 16:23:03 +0900

    try to run pytest w/ github actions

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -12,3 +12,8 @@ def test_handler(context):
     assert handler({"payload": "hello world!"}, context).startswith(
         "Hello from AWS Lambda using Python"
     )
+
+def test_to_fail(context):
+    assert handler({"payload": "hello world!"}, context).startswith(
+        "Hello from AWS Lambda using Python!!"
+    )
